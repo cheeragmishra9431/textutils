3ec18d2627b9c2a50ddfe73f8c85b17b35fc67b6
cheeragmishra9431 <waytogocheerag@gmail.com>
2021-12-20 15:18:55 +0530

    today on 20th dec 2021

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-
+var name="harry";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <nav>
+      <li>
+        HOME
+      </li>
+      <li>
+        About
+      </li>
+      <li>
+        Contact
+      </li>
+      <div>
+        {name}
+      </div>
+    </nav>
+  </>
   );
 }
 
